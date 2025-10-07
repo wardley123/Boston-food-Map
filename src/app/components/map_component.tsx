a0dcdf9fd9ebd54a44c074102a8d1bb71da1a1ad
wardley123 <wardleylervandal@gmail.com>
2025-10-07 18:52:16 -0400

    created map

---
+++ src/app/components/map_component.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps'
+
+export default function GoogleMap() {
+    const position = {lat: 42.407211, lng: -71.382439}
+    return(
+        
+    <APIProvider apiKey= {process.env.NEXT_PUBLIC_MAP_API_KEY}>
+      <div style={{ height: '500px', width: '100%' }}>
+        <Map
+          defaultCenter={position}
+          defaultZoom={12}
+          >
+
+          <Marker position={position} />
+
+        </Map>
+      </div>
+    </APIProvider>
+    )
+    
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import GoogleMap from "./components/map_component";
 
 export default function Home() {
   return (
     <div>
+      <GoogleMap />
+
       <h1>Home page</h1>
       <h1>hellos</h1>
     </div>
